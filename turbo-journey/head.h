8d7aabe33fd699f33b4bfb35376e872af5ad4591
Issa Chanzi <issachanzi@hotmail.com>
2018-07-16 12:55:03 +1000

    Organised Banjo's code

--- turbo-journey/head.h
+++ turbo-journey/head.h
@@ -1,9 +1,20 @@
 
-int s2 =
-int s3
+int s2 = 5;
+int s3 = 6;
+int out = 7;
+double rscale = 3000.0
+double gscale = 3500.0
+double bscale = 2500.0
 
-void setup(){
+void sensorLeft(void);
+void sensorRight(void);
+void sensorForward    (void);
+double getRed(void);
+double getBlue(void);
+double getGreen(void);
 
-
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
