7c23550122f15758245569abfde72a181c02f590
Issa Chanzi <issachanzi@hotmail.com>
2018-07-16 13:02:00 +1000

    REorganised again

--- turbo-journey/head.h
+++ turbo-journey/head.h
@@ -1,20 +1,25 @@
 
+//pins
+int sensorserv = 3;
+
 int s2 = 5;
 int s3 = 6;
 int out = 7;
+
+
 double rscale = 3000.0
 double gscale = 3500.0
 double bscale = 2500.0
 
+
+
 void sensorLeft(void);
 void sensorRight(void);
-void sensorForward    (void);
+void sensorForward(void);
 double getRed(void);
 double getBlue(void);
 double getGreen(void);
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+void goForward(void);
+void goLeft(void);
+void goRight(void);
 
--- turbo-journey/turbo-journey.ino
+++ turbo-journey/turbo-journey.ino
@@ -1,7 +1,30 @@
 #include "head.h"
 
-void loop(){
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
 
-  
+void loop() {
+  // put your main code here, to run repeatedly:
+ int blackleft;
+ int blackright;
+ int blackforward;
+
+  sensorLeft();
+  if(getRed() + getBlue() + getGreen() < 40){
+    blackleft = 1;
+  }
+  else blackleft = 0;
+  sensorForward();
+  if (getRed() + getBlue() + getGreen() < 40){
+    blackforward = 1;
+  }
+    else blackforward = 0;
+    sensorRight ();
+    if (getRed() + getBlue() + getGreen() < 40){
+      blackright = 1;
+  }
+
 }
 
