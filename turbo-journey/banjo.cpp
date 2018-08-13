49060db3a1a51823981bd6a1ac26560e81dd77b5
Issa Chanzi <issachanzi@hotmail.com>
2018-08-14 09:16:58 +1000

    Changed to slip turns

--- turbo-journey/banjo.cpp
+++ turbo-journey/banjo.cpp
@@ -8,7 +8,7 @@
   }
   void goLeft(void){
       digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
+      digitalWrite(in2, LOW);
       digitalWrite(in3, HIGH);
       digitalWrite(in4, LOW);
   }
@@ -16,7 +16,7 @@
       digitalWrite(in1, HIGH);
       digitalWrite(in2, LOW);
       digitalWrite(in3, LOW);
-      digitalWrite(in4, HIGH);
+      digitalWrite(in4, LOW);
   }
 
   void brakes(void){
